5f9fa9edff2c163e5c23de52b944909f6032ed09
Thomas Hanke <thomas.hanke@iwm.fraunhofer.de>
2024-02-28 08:54:43 +0100

    fixed an auth issue

--- ckanext/fuseki/logic/auth.py
+++ ckanext/fuseki/logic/auth.py
@@ -3,16 +3,17 @@
 import ckan.plugins as p
 from ckan.logic.auth.get import task_status_show
 
-def jena_auth(context, data_dict, privilege="resource_update"):
+
+def jena_auth(context, data_dict, privilege="package_update"):
     if "id" not in data_dict:
-        data_dict["id"] = data_dict.get("resource_id")
+        data_dict["id"] = data_dict.get("resource_ids")[0]
     user = context.get("user")
     authorized = p.toolkit.check_access(privilege, context, data_dict)
     if not authorized:
         return {
             "success": False,
             "msg": p.toolkit._(
-                "User {0} not authorized to update resource {1}".format(
+                "User {0} not authorized to update package {1}".format(
                     str(user), data_dict["id"]
                 )
             ),
@@ -21,13 +22,13 @@ def jena_auth(context, data_dict, privilege="resource_update"):
         return {"success": True}
 
 
-def jena_create(context, data_dict):
-    if "resource" in data_dict and data_dict["resource"].get("package_id"):
-        data_dict["id"] = data_dict["resource"].get("package_id")
-        privilege = "package_update"
-    else:
-        privilege = "resource_update"
-    return jena_auth(context, data_dict, privilege=privilege)
+# def jena_create(context, data_dict):
+#     if "resource" in data_dict and data_dict["resource"].get("package_id"):
+#         data_dict["id"] = data_dict["resource"].get("package_id")
+#         privilege = "package_update"
+#     else:
+#         privilege = "resource_update"
+#     return jena_auth(context, data_dict, privilege=privilege)
 
 
 def fuseki_delete(context, data_dict):
@@ -47,14 +48,15 @@ def fuseki_update(context, data_dict):
         privilege = "resource_update"
     return jena_auth(context, data_dict, privilege=privilege)
 
+
 def fuseki_update_status(context, data_dict):
     return task_status_show(context, data_dict)
 
+
 def get_auth_functions():
     return {
-                "jena_create": jena_create,
-                "fuseki_delete": fuseki_delete,
-                "fuseki_update": fuseki_update,
-                "fuseki_update_status": fuseki_update_status,
-                "jena_search_sparql": jena_search_sparql,
-            }
+        "fuseki_delete": fuseki_delete,
+        "fuseki_update": fuseki_update,
+        "fuseki_update_status": fuseki_update_status,
+        "jena_search_sparql": jena_search_sparql,
+    }
